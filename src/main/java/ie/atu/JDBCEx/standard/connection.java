4c622464d3f0edee51cf69e97cdb438f51fe4d28
G00403126 <G00403126@atu.ie>
2023-03-23 12:20:21 +0000

    CONNECTED SQL WITH CODE

--- src/main/java/ie/atu/JDBCEx/standard/connection.java
+++ src/main/java/ie/atu/JDBCEx/standard/connection.java
@@ -1,4 +1,33 @@
 package ie.atu.JDBCEx.standard;
 
+
+import java.sql.*;
+
 public class connection {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        try
+        {
+            // Load the driver class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Create a connection to the database
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/exampledatabase", "root", "root");
+            System.out.println("Connection made to connection pool");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally
+        {
+            // Close the connection when finished
+            if (connection != null) {
+                try
+                {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
